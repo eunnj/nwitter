7da9b39f264c7aacb95861a72700ffa4349925d3
user <yenje7@naver.com>
2022-03-10 14:46:42 +0900

    Log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,27 @@
 import React, {useState} from "react";
 import { HashRouter as Router , Route, Routes} from "react-router-dom";
+import Profile from "routes/Profile";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "./Navigation";
+import {Navigate} from "react-router-dom";
+
 
 const AppRouter = ({isLoggedin})=>{
     return(
         <Router>
+            {isLoggedin&&<Navigation/>}
             <Routes>
                 {isLoggedin ? (
                 <>
                 <Route exact path="/" element ={<Home/>}></Route>
+                <Route exact path="/profile" element ={<Profile/>}></Route>
                 </>
                 ):(
+                <>
                 <Route exact path="/" element ={<Auth/>}></Route>
+                <Route path="*" element ={<Navigate replace to="/"/>}></Route>
+                </>
                 )}
             </Routes>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,12 @@
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
-export default () => <span>Profile</span>;
+export default () => {
+    const navigate = useNavigate();
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate("/");
+    };
+    return (<><button onClick={onLogOutClick}>Log Out</button></>);
+};
